baccb13506a8dd362e28f65eb966aacd4a50b9dd
Fernando Contreras <fercreek@gmail.com>
2017-11-02 01:02:53 -0600

    creating admin interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates :role, inclusion: %w(admin user)
 
   before_create :generate_confirmation_token
 
@@ -11,5 +12,9 @@ class User < ApplicationRecord
 
   def confirmed?
     self.confirmed_at.present?
+  end
+
+  def admin?
+    self.role == 'admin'
   end
 end
